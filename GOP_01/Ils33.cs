1f7893411711d01d50fc94fb07597d45e3cfc8ef
Lasse Meggele <lassemeggele@outlook.com>
2017-02-05 11:37:49 +0100

    Updated Ils33

--- GOP_01/Ils33.cs
+++ GOP_01/Ils33.cs
@@ -1,35 +1,49 @@
-﻿using System;
-using static System.Console;
-using System.Globalization;
+﻿using static System.Console;
 
 namespace GOP_01
 {
     internal class Ils33
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             const int momssats = 25;
+
+            #region Intastning af data
             WriteLine("Indtast et varenavn: ");
             var vareNavn = ReadLine();
 
             WriteLine("Indtast en pris pr. vare uden moms: ");
-            var beloeb = double.Parse(ReadLine());
+            var s = ReadLine();
+            if (string.IsNullOrEmpty(s))
+                s = "0";
+
+            var beloeb = double.Parse(s);
 
             WriteLine("Indtast antal: ");
-            var antal = int.Parse(ReadLine());
+            s = ReadLine();
+            if (string.IsNullOrEmpty(s))
+                s = "1";
 
+            var antal = int.Parse(s);
+            #endregion
+
+            #region Beregninger
             var samletPris = beloeb * antal;
             var moms = samletPris * momssats / 100;
             var ialt = samletPris + moms;
+            #endregion
+
+            #region Visning af beregninger
             WriteLine();
             WriteLine($"Varenavn: {vareNavn}\r\n" +
-                      $"Styk pris: {beloeb:c}\r\n" +
-                      $"Antal: {antal}\r\n" +
+                      $"Styk pris: {beloeb:C}\r\n" +
+                      $"Antal: {antal:N}\r\n" +
                       $"Samlet Pris: {samletPris:c}\r\n" +
                       $"Moms: {moms:c}\r\n" +
                       $"Pris i alt: {ialt:c}\r\n");
             WriteLine("Tryk på Enter for at afslutte.");
             ReadLine();
+            #endregion
         }
     }
 }
