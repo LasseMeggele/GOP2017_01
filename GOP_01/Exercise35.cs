98f8f60cb9fe6b256f8a37e43e30f7b7a2461df6
lasse <lasse@meggele.dk>
2017-02-06 01:05:16 +0100

    Added Exercise35 to solution

---
+++ GOP_01/Exercise35.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace GOP_01
+{
+    internal class Exercise35
+    {
+        public static void Main(string[] args)
+        {
+            int w, x, y, z;
+            var resultList = new List<bool>();
+
+            while (true)
+            {
+                WriteLine("Indtast w: ");
+                resultList.Add(int.TryParse(ReadLine(), out w));
+                WriteLine("Indtast x: ");
+                resultList.Add(int.TryParse(ReadLine(), out x));
+                WriteLine("Indtast y: ");
+                resultList.Add(int.TryParse(ReadLine(), out y));
+                WriteLine("Indtast z: ");
+                resultList.Add(int.TryParse(ReadLine(), out z));
+
+                if (resultList.Contains(false))
+                {
+                    WriteLine($"Alle værdier skal udfyldes. Prøv igen eller tryk CTRL+C for at afbryde.");
+                    ReadKey();
+                    resultList.Clear();
+                    Clear();
+                    continue;
+                }
+                break;
+            }
+
+            WriteLine($"Produktet er: {w * x * y * z}");
+            ReadKey();
+        }
+    }
+}
