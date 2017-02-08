f34839d55c8a3d4acf64a5ad407d28ad873f8c88
lasse <lasse@meggele.dk>
2017-02-08 22:44:47 +0100

    Added Ils41 and Ils42 classes to solution

---
+++ GOP_01/Ils41.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Ils41.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the Ils41 type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GOP_01
+{
+    using static System.Console;
+
+    /// <summary>
+    /// The Ils41 class.
+    /// </summary>
+    internal class Ils41
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        private static void Main()
+        {
+            int weight;
+
+            while (true)
+            {
+                WriteLine("Angiv vægten af forsendelsen i gram: ");
+                var result = int.TryParse(ReadLine(), out weight);
+
+                if (!result)
+                {
+                    WriteLine("Angiv vægten som et positivt heltal, eller afbryd med CTRL+C. ");
+                    ReadKey();
+                    continue;
+                }
+
+                break;
+            }
+
+            WriteLine(weight > 1000
+                ? $"Forsendelsen er en pakke på {(double)weight / 1000:N2} kg"
+                : $"Forsendelsen er et brev på {weight:N0} g");
+        }
+    }
+}
---
+++ GOP_01/Ils42.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Ils42.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the Ils42 type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GOP_01
+{
+    using static System.Console;
+
+    /// <summary>
+    /// The Ils42 class.
+    /// </summary>
+    internal class Ils42
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        private static void Main()
+        {
+            double purchase;
+            int discount;
+
+            while (true)
+            {
+                WriteLine($"Angiv samlet pris for at beregne rabat. ");
+                var result = double.TryParse(ReadLine(), out purchase);
+
+                if (!result)
+                {
+                    WriteLine($"Angiv den samlede pris for at beregne rabat, eller afbryd ved at trykke på CTRL+C. ");
+                    ReadKey();
+                    continue;
+                }
+
+                break;
+            }
+
+            if (purchase > 2000)
+                discount = 10;
+            else if (purchase > 1000 && purchase <= 2000)
+                discount = 5;
+            else
+                discount = 0;
+
+            WriteLine($"{"Købspris før rabat:", -20} {purchase,6:C2}");
+            WriteLine($"{"Samlet rabat:", -20} {purchase * discount / 100,6:C2}");
+            WriteLine($"{"Pris efter rabat:", -20} {purchase - purchase * discount / 100,6:C2}");
+        }
+    }
+}
