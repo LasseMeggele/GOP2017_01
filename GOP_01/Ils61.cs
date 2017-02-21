ece19c520ffe3c61c73725687ea42b974404925b
Lasse Meggele <lassemeggele@outlook.com>
2017-02-21 16:05:47 +0100

    Added new excercises

---
+++ GOP_01/Ils61.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GOP_01
+{
+    internal class Ils61
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(BeregnSekunder(24,35,46));
+            Console.WriteLine(BeregnSekunder(40,0,0));
+        }
+
+        private static int BeregnSekunder(int timer, int minutter, int sekunder)
+        {
+            return timer * (int)Math.Pow(60, 2) + minutter * 60 + sekunder;
+        }
+    }
+}
