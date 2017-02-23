685a0ad4de6e2f04a92faeb7c25a6121b18b730a
Lasse Meggele <lassemeggele@outlook.com>
2017-02-23 15:07:45 +0100

    New excercises added.

---
+++ GOP_01/Ils66.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOP_01
+{
+    class Ils66
+    {
+        static void Main(string[] args)
+        {
+            var startRabbitPairs = 4;
+            var month = 0;
+
+
+
+        }
+
+
+
+
+        private static int RabbitMachine(int rabbitPair)
+        {
+            return rabbitPair * 3;
+        }
+    }
+}
