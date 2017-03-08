0568780f56d855af97da98658342b8e8738a9f28
Lasse Meggele <lassemeggele@outlook.com>
2017-03-08 13:13:56 +0100

    test

--- GOP_01/Ils91.cs
+++ GOP_01/Ils91.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GOP_01
 {
@@ -15,9 +16,12 @@ namespace GOP_01
                 manufacturer: "LENOVO", 
                 model: "Thinkpad X260", 
                 description: null, 
+                price: 6500.0m,
+                purchaseDate: new DateTime(2017, 1, 2), 
                 serialnumber: "234asdf345", 
                 uuid: null,
-                macAddressList: null
+                macAddressList: null,
+                serviceContract: new ServiceContract()
             );
             var c3 = new Computer();
 
@@ -26,11 +30,15 @@ namespace GOP_01
             c2.MacAddress = macs;
             c2.MacAddress.Add("34-56-78-90-AB-CD");
             
+            var u1 = new User("lamg", "Lasse Meggele", "IOS", null, new List<AssetItem>(){c2});
+
 
             Console.WriteLine(c1.ToString());
             Console.WriteLine(c2.ToString());
             Console.WriteLine(c3.ToString());
 
+            Console.WriteLine(u1.ToString());
+            Console.WriteLine(u1.AssetsItems);
 
         }
     }
